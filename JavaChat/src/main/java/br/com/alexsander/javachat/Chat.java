77b795e94ed5669f62b1517c4d6082b2cd322c61
Alexsander Oliveira <alexsander1996@yahoo.com.br>
2020-11-30 20:12:09 -0300

    Criação do projeto Java - Maven

---
+++ JavaChat/src/main/java/br/com/alexsander/javachat/Chat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.alexsander.javachat;
+
+/**
+ *
+ * @author alexs
+ */
+public class Chat {
+    
+    public static void main(String[] args) {
+        
+    }
+}
